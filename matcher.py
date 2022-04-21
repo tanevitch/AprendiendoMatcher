f22693b36d2343999ac718452425f749862a4168
tanevitch <lubatane@gmail.com>
2022-04-21 13:30:39 -0300

    matcher modificado

--- matcher.py
+++ matcher.py
@@ -1,28 +1,24 @@
 import spacy
 from spacy.matcher import Matcher
 
-# Modelo de precisión en inglés
-nlp = spacy.load("en_core_web_trf")
-
-class AprendiendoMatcher:
-    matcher = Matcher(nlp.vocab)
+NLP = spacy.load("en_core_web_trf")
+class SimpleMatcher:
+    matcher = Matcher(NLP.vocab)
     
-    def __init__(self, expresion):
-        self.matcher.add("patron", expresion)
+    def addPattern(self, pattern_name: str, expression: list):
+        self.matcher.add(pattern_name, expression)
+
+    def removePattern(self,pattern_name:str):
+        self.matcher.remove(pattern_name)
+
+    def getMatches(self, sentence: str):
+        processed_sentence = NLP(sentence)
+
+        matches = {}
         
-    def setMatcher(self, expresion) -> None:
-        '''
-        Por cuestiones de simplicidad, este matcher va a trabajar con un solo patrón a la vez. Por esta razón, cada vez que se setea uno nuevo se borra el anterior
-        '''
-        self.matcher.remove("patron")
-        self.matcher.add("patron", expresion)
-          
-    def procesarOracionLenguajeNatural(self, oracion_lenguaje_natural) -> list[str]:
-        '''
-        Procesa la oración con NLP, porque el matcher espera eso como parámetro. Luego, la procesa con el matcher y retorna una lista de strings que son las palabras que matchearon con el patrón seteado
-        '''
-        oracion_procesada = nlp(oracion_lenguaje_natural)
-        matches= self.matcher(
-            oracion_procesada, as_spans=True
-        )
-        return list(map(lambda each: str(each), matches))
+        for match_id, start, end in self.matcher(processed_sentence):
+            string_id = NLP.vocab.strings[match_id]  
+            span = processed_sentence[start:end]  
+            matches[string_id] = span.text if not string_id in matches.keys() else [matches[string_id]].extend(span.text)
+
+        return matches
